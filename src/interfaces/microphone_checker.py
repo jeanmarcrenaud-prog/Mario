00d631ad40d81f22ac1373edd078e90117963f38
jeanmarcrenaud-prog <jeanmarc.renaud@gmail.com>
2025-12-10 23:48:15 +0100

    class

--- src/interfaces/microphone_checker.py
+++ src/interfaces/microphone_checker.py
@@ -1,7 +1,7 @@
 # interfaces/microphone_checker.py
 from abc import ABC, abstractmethod
 
-class MicrophoneChecker(ABC):
+class Microphone_Checker(ABC):
     @abstractmethod
     def is_microphone_available(self) -> bool:
         pass
